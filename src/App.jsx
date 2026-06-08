f009c86a64027d9a038b8ba035d98876bbc5441d
aishwarya <kurup.aishwarya97@gmail.com>
2026-06-08 21:51:08 +0200

    Added the text card

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { NoteAPI } from "./api/note-api";
 import { Outlet } from "react-router-dom";
 import { Header } from "./components/Header/Header";
+import { useDispatch } from "react-redux";
+import { setNoteList } from "./store/notes/notes-slice";
+import { useEffect } from "react";
 
 export function App() {
+  const dispatch = useDispatch();
+
+  async function fetchAllNotes(){
+    const noteList = await NoteAPI.fetchAll();
+    dispatch(setNoteList(noteList));
+  }
+
+  useEffect(() => {
+    fetchAllNotes();
+  }, []);
+
   return <div>
     <Header/>
 
--- src/pages/NoteBrowse/NoteBrowse.jsx
+++ src/pages/NoteBrowse/NoteBrowse.jsx
@@ -1,3 +1,14 @@
+import { TextCard } from "../../components/TextCard/TextCard";
+
 export function NoteBrowse(props){
-    return <>NoteBrowse</>;
+    return <>
+        <TextCard
+            title="Super note"
+            subtitle="01/01/2026"
+            content="bla bla bla bla bla blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla blabla bla bla"
+            onClick={()=> alert("OnClick !")}
+            onClickTrash={()=> alert("OnClickTrash !")}
+
+        />
+    </>;
 }
